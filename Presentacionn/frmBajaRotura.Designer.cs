b729738a649a844f9bcdba8b873cc34389f45b58
24fer <24fer@COLINABO>
2022-11-06 23:10:53 -0300

    Ver 0.3.3.2 Ahora si arranca todo

--- Presentacionn/frmBajaRotura.Designer.cs
+++ Presentacionn/frmBajaRotura.Designer.cs
@@ -28,24 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cbxRotura = new System.Windows.Forms.ComboBox();
             this.btnEliminarRotura = new System.Windows.Forms.Button();
-            this.lblRotura = new System.Windows.Forms.Label();
-            this.lblDescripcion = new System.Windows.Forms.Label();
             this.lblIdEquipo = new System.Windows.Forms.Label();
-            this.txtIdEquipo = new System.Windows.Forms.TextBox();
-            this.txtEquipoRoto = new System.Windows.Forms.TextBox();
-            this.lblEquipoRoto = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mtxtRotura = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
-            // 
-            // cbxRotura
-            // 
-            this.cbxRotura.FormattingEnabled = true;
-            this.cbxRotura.Location = new System.Drawing.Point(274, 60);
-            this.cbxRotura.Name = "cbxRotura";
-            this.cbxRotura.Size = new System.Drawing.Size(121, 21);
-            this.cbxRotura.TabIndex = 9;
             // 
             // btnEliminarRotura
             // 
@@ -60,64 +46,22 @@
             this.btnEliminarRotura.UseVisualStyleBackColor = false;
             this.btnEliminarRotura.Click += new System.EventHandler(this.btnEliminarRotura_Click);
             // 
-            // lblRotura
-            // 
-            this.lblRotura.AutoSize = true;
-            this.lblRotura.Location = new System.Drawing.Point(217, 63);
-            this.lblRotura.Name = "lblRotura";
-            this.lblRotura.Size = new System.Drawing.Size(53, 13);
-            this.lblRotura.TabIndex = 12;
-            this.lblRotura.Text = "ID Rotura";
-            // 
-            // lblDescripcion
-            // 
-            this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(205, 127);
-            this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
-            this.lblDescripcion.TabIndex = 13;
-            this.lblDescripcion.Text = "Descripción";
-            // 
             // lblIdEquipo
             // 
             this.lblIdEquipo.AutoSize = true;
-            this.lblIdEquipo.Location = new System.Drawing.Point(24, 63);
+            this.lblIdEquipo.Location = new System.Drawing.Point(156, 146);
             this.lblIdEquipo.Name = "lblIdEquipo";
             this.lblIdEquipo.Size = new System.Drawing.Size(54, 13);
             this.lblIdEquipo.TabIndex = 7;
             this.lblIdEquipo.Text = "ID Equipo";
             // 
-            // txtIdEquipo
+            // mtxtRotura
             // 
-            this.txtIdEquipo.Location = new System.Drawing.Point(82, 60);
-            this.txtIdEquipo.Name = "txtIdEquipo";
-            this.txtIdEquipo.Size = new System.Drawing.Size(100, 20);
-            this.txtIdEquipo.TabIndex = 8;
-            this.txtIdEquipo.TextChanged += new System.EventHandler(this.txtIdEquipo_TextChanged);
-            // 
-            // txtEquipoRoto
-            // 
-            this.txtEquipoRoto.Location = new System.Drawing.Point(82, 124);
-            this.txtEquipoRoto.Name = "txtEquipoRoto";
-            this.txtEquipoRoto.Size = new System.Drawing.Size(100, 20);
-            this.txtEquipoRoto.TabIndex = 15;
-            // 
-            // lblEquipoRoto
-            // 
-            this.lblEquipoRoto.AutoSize = true;
-            this.lblEquipoRoto.Location = new System.Drawing.Point(10, 127);
-            this.lblEquipoRoto.Name = "lblEquipoRoto";
-            this.lblEquipoRoto.Size = new System.Drawing.Size(66, 13);
-            this.lblEquipoRoto.TabIndex = 14;
-            this.lblEquipoRoto.Text = "Equipo Roto";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(274, 124);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 90);
-            this.textBox1.TabIndex = 16;
+            this.mtxtRotura.Location = new System.Drawing.Point(217, 138);
+            this.mtxtRotura.Mask = "000000";
+            this.mtxtRotura.Name = "mtxtRotura";
+            this.mtxtRotura.Size = new System.Drawing.Size(100, 20);
+            this.mtxtRotura.TabIndex = 12;
             // 
             // frmBajaRotura
             // 
@@ -125,14 +69,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(228)))), ((int)(((byte)(244)))));
             this.ClientSize = new System.Drawing.Size(494, 508);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtEquipoRoto);
-            this.Controls.Add(this.lblEquipoRoto);
-            this.Controls.Add(this.lblDescripcion);
-            this.Controls.Add(this.lblRotura);
+            this.Controls.Add(this.mtxtRotura);
             this.Controls.Add(this.btnEliminarRotura);
-            this.Controls.Add(this.cbxRotura);
-            this.Controls.Add(this.txtIdEquipo);
             this.Controls.Add(this.lblIdEquipo);
             this.Name = "frmBajaRotura";
             this.Text = "frmBajaRotura";
@@ -142,14 +80,8 @@
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox cbxRotura;
         private System.Windows.Forms.Button btnEliminarRotura;
-        private System.Windows.Forms.Label lblRotura;
-        private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.Label lblIdEquipo;
-        private System.Windows.Forms.TextBox txtIdEquipo;
-        private System.Windows.Forms.TextBox txtEquipoRoto;
-        private System.Windows.Forms.Label lblEquipoRoto;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox mtxtRotura;
     }
 }
