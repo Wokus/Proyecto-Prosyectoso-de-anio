7ca0e8915c25752ca6f9b01819f4591340618161
24fer <24fer@COLINABO>
2022-11-06 23:54:15 -0300

    alta espacio

--- Dominio/dEspacio.cs
+++ Dominio/dEspacio.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
+using Persistencia;
 
 namespace Dominio
 {
-    class dEspacio
+    public class dEspacio
     {
+        public int  altaEspacio(eEspacio unE)
+        {
+            pEspacio p = new pEspacio();
+            int token = p.altaEspacio(unE);
+        }
     }
 }
--- Persistencia/pEspacio.cs
+++ Persistencia/pEspacio.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
+using MySql.Data.MySqlClient;
 
 namespace Persistencia
 {
-    class pEspacio
+    public class pEspacio : clsPersistencia
     {
+        public int altaEspacio(eEspacio unE)
+        {
+            int token = 0;
+            eUsuario elAdmin = new eUsuario();
+            elAdmin = null;
+            string consultaSQL = "SELECT * FROM `espacio` WHERE nro = '" + unE.nombreEspacio+ "';";
+            MySqlDataReader fila = ejecutarYdevolver(consultaSQL);
+
+            while (fila.Read())
+            {
+                consultaSQL = "INSERT INTO espacio VALUES('" + unE.numeroEspacio + "','" + unE.nombreEspacio + "');";
+                fila = ejecutarYdevolver(consultaSQL);
+
+            }
+    
+            return token;
+        }
     }
 }
