15c9f0b68edecc36f53d5bdc6343244f4af9c4b4
Crepi <LuquitaCrespi@gmail.com>
2022-11-06 23:35:12 -0300

    Ver 0.3.2.3

---
+++ Presentacionn/frmListadoEspacio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Entidades;
+
+namespace Presentacionn
+{
+    public partial class frmListadoEspacio : Form
+    {
+        public frmListadoEspacio()
+        {
+            InitializeComponent();
+        }
+
+        private void frmListadoEspacio_Load(object sender, EventArgs e)
+        {
+            dEspacio unDE = new dEspacio();
+
+
+        }
+    }
+}
